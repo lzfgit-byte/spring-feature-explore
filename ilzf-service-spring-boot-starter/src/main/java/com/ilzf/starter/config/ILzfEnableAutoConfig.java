0790114d9dd83596adb0d119e25abf34bb8e91f0
lzfgit-byte <1807409742@qq.com>
2024-03-03 11:15:23 +0800

    init

--- ilzf-service-spring-boot-starter/src/main/java/com/ilzf/starter/config/ILzfEnableAutoConfig.java
+++ ilzf-service-spring-boot-starter/src/main/java/com/ilzf/starter/config/ILzfEnableAutoConfig.java
@@ -3,6 +3,7 @@ package com.ilzf.starter.config;
 import com.ilzf.starter.properties.ILzfServerProperties;
 import com.ilzf.starter.service.ILzfService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -14,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(ILzfService.class)
 @EnableConfigurationProperties(ILzfServerProperties.class)//注册该类同级的bean，让spring自动扫描
 public class ILzfEnableAutoConfig {
+    // 有这个bean的时候注入
+    @Qualifier("ILzfServerProperties")
     @Autowired
     ILzfServerProperties properties;
 
