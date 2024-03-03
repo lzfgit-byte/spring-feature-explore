020d78dbfb21c8d4033d9c2fc1cc12a6033e4ed3
lzfgit-byte <1807409742@qq.com>
2024-03-03 11:31:43 +0800

    init

--- spring-feature-anno/src/main/java/com/ilzf/anno/AnnoApplication.java
+++ spring-feature-anno/src/main/java/com/ilzf/anno/AnnoApplication.java
@@ -1,4 +1,4 @@
-package com.ilzf;
+package com.ilzf.anno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnnoApplication {
     public static void main(String[] args) {
         SpringApplication.run(AnnoApplication.class, args);
+        System.out.println("http://localhost:8080");
     }
 }
