a9f43173c9e6b6f52cdbecbdd60b963376f35cd4
lzfgit-byte <1807409742@qq.com>
2024-03-03 11:54:35 +0800

    init

--- spring-feature-anno/src/main/java/com/ilzf/anno/AnnoApplication.java
+++ spring-feature-anno/src/main/java/com/ilzf/anno/AnnoApplication.java
@@ -3,13 +3,21 @@ package com.ilzf.anno;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebMvc
-public class AnnoApplication {
+public class AnnoApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(AnnoApplication.class, args);
         System.out.println("http://localhost:8080");
         System.out.println("http://localhost:8080/doc.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
--- spring-feature-anno/src/main/java/com/ilzf/anno/controller/IndexController.java
+++ spring-feature-anno/src/main/java/com/ilzf/anno/controller/IndexController.java
@@ -1,21 +1,29 @@
 package com.ilzf.anno.controller;
 
+import com.ilzf.starter.service.ILzfService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "首页")
+@Api(tags = "测试starter")
 @RestController("/")
 public class IndexController {
 
+    @Autowired
+    ILzfService iLzfService;
+
 
     @ApiImplicitParam(name = "name", value = "姓名", required = true)
-    @ApiOperation(value = "向客人问好")
+    @ApiOperation(value = "获取配置")
     @GetMapping("/")
-    public String index() {
-        return "132";
+    public String index(String name) {
+        iLzfService.say();
+        iLzfService.sayProp();
+        return name;
     }
+
 
 }
