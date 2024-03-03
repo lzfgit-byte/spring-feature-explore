10dc5550633a5aa7cece089c202429f2434bb7be
lzfgit-byte <1807409742@qq.com>
2024-03-03 10:34:48 +0800

    init

---
+++ spring-boot-starter-izfserver/src/main/java/com/ilzf/ILzfServerApplication.java
@@ -0,0 +1,11 @@
+package com.ilzf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILzfServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ILzfServerApplication.class, args);
+    }
+}
