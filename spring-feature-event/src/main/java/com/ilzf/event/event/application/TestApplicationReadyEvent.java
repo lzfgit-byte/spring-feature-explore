d9f6e1564d5de7ae026ab94597c57764b6140d0f
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-06 16:01:38 +0800

    application event

--- spring-feature-event/src/main/java/com/ilzf/event/event/application/TestApplicationReadyEvent.java
+++ spring-feature-event/src/main/java/com/ilzf/event/event/application/TestApplicationReadyEvent.java
@@ -5,6 +5,16 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * -try {
+ * -          Duration timeTakenToReady = Duration.ofNanos(System.nanoTime() - startTime);
+ * -          listeners.ready(context, timeTakenToReady);
+ * -          return context;
+ * <p>
+ * this.callRunners(context, applicationArguments);之后运行，
+ * 从此之后，启动的所有事项已经处理完毕，
+ * 万籁俱寂
+ */
 @Component
 public class TestApplicationReadyEvent implements ApplicationListener<ApplicationReadyEvent> {
     @Override
--- spring-feature-event/src/main/java/com/ilzf/event/event/application/TestApplicationStartedEvent.java
+++ spring-feature-event/src/main/java/com/ilzf/event/event/application/TestApplicationStartedEvent.java
@@ -5,6 +5,11 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * - listeners.started(context, timeTakenToStartup);
+ * - this.callRunners(context, applicationArguments);
+ * 启动完成后第一时间通知，不算在启动时间里
+ */
 @Component
 public class TestApplicationStartedEvent implements ApplicationListener<ApplicationStartedEvent> {
     @Override
