c003e66775aae3950b695470d92574b4d2bf3dff
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-07 11:36:48 +0800

    annotations

--- spring-feature-anno/src/main/java/com/ilzf/anno/annotations/TestValue.java
+++ spring-feature-anno/src/main/java/com/ilzf/anno/annotations/TestValue.java
@@ -1,10 +1,7 @@
 package com.ilzf.anno.annotations;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.ServletContextResource;
 
@@ -62,16 +59,51 @@ public class TestValue {
      * 因此，在规划的时候，应该尽可能规划好资源文件所在的路径，尽量避免使用classpath*。
      * <p>
      * 效果类似于this.getClass().getResource("/").getPath();
+     * <p>
+     * <p>
+     * resource 都是可以直接创建的，使用@value只是一种简便的方式
      */
     @Value("classpath:application.yml")
     public ClassPathResource file;
     @Value("application.yml")
     public FileSystemResource fileSystemResource;
+
+//    String filename = "/var/tmp/mysql.png";
+//    File file = new File(filename);
+//    InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+//    public InputStreamResource inputStreamResource;
+
+
+    /**
+     * 一种ByteArrayResource的使用方式
+     * -   @RequestMapping("/file")
+     * -   @ResponseBody
+     * -   public String testDemo(@RequestParam("uploadFile") MultipartFile file,StandardMultipartHttpServletRequest request) throws IOException {
+     * -		String url = "https://www.uploadFile.com/upload";
+     * -		RestTemplate template = new RestTemplate();
+     * -        HttpHeaders headers = new HttpHeaders();
+     * -        headers.add("Content-Type", "multipart/form-data");
+     * -        headers.add("Content-Length",String.valueOf(file.getSize()));
+     * -        MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
+     * -        【hi there】
+     * -        ByteArrayResource resource = new ByteArrayResource(file.getBytes()) {
+     * -            @Override
+     * -            public String getFilename() {
+     * -                return file.getOriginalFilename();
+     * -            }
+     * -        };
+     * -        parts.add("file",resource);
+     * -        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(parts, headers);
+     * -        ResponseEntity<String> responseEntity = template.postForEntity(url,httpEntity,String.class);
+     * -		return "Hello World!";
+     * -   }
+     */
+    public ByteArrayResource byteArrayResource;
     /**
      * 这是 ServletContext 资源的 Resource 实现，它解释相关 Web 应用程序根目录中的相对路径。
+     * spring 时代常用
      */
-//    @Value("")
-    public ServletContextResource servletContextResource;
+//    public ServletContextResource servletContextResource = new ServletContextResource(application, 'WEB_INF/book.xml');
     /**
      * 注入网站资源
      * UrlResource
