9f5c5ac0e12516b469d8477e67615f33cef7895d
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-11 15:14:02 +0800

    junit

--- spring-feature-test/src/main/test/ApplicationTest.java
+++ spring-feature-test/src/main/test/ApplicationTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
-import java.awt.*;
 import java.util.Random;
 
 import static org.junit.jupiter.api.condition.JRE.*;
