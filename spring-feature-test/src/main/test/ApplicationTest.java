069838bda92680d50fd8fe006fb2499ee6034867
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-11 14:02:28 +0800

    junit

--- spring-feature-test/src/main/test/ApplicationTest.java
+++ spring-feature-test/src/main/test/ApplicationTest.java
@@ -1,10 +1,14 @@
 
 import com.ilzf.testjunit.JunitApplication;
 import com.ilzf.testjunit.service.JunitService;
-import org.junit.jupiter.api.Test;
+import com.ilzf.util.LogUtil;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
+
+import java.util.Random;
+
 
 @SpringBootTest(classes = JunitApplication.class)
 public class ApplicationTest {
@@ -12,8 +16,38 @@ public class ApplicationTest {
     @Autowired
     JunitService junitService;
 
+    @BeforeAll
+    public static void beforeAll() {
+        LogUtil.log("test beforeAll");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        LogUtil.log("test afterAll");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        LogUtil.log("test beforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        LogUtil.log("test AfterEach");
+    }
+
+
     @Test
     public void test() {
         Assert.isTrue(junitService.say(), "不通过");
+        Assertions.assertTrue(junitService.say());
+    }
+
+    @Test
+    public void test2() {
+        Random random = new Random();
+        int a = random.nextInt(10);
+        LogUtil.log(a);
+        Assertions.assertTrue(a > 4, "小于四");
     }
 }
--- spring-feature-util/src/main/java/com/ilzf/util/LogUtil.java
+++ spring-feature-util/src/main/java/com/ilzf/util/LogUtil.java
@@ -4,4 +4,8 @@ public class LogUtil {
     public static void log(String... args) {
         System.out.println(StringUtil.wrapperLog(args));
     }
+
+    public static void log(int args) {
+        System.out.println(args);
+    }
 }
