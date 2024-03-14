6529d922c93f3cfd721c740caa4837ebd96b9fee
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-14 11:58:23 +0800

    spring-cloud

--- spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/ServerOneApplication.java
+++ spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/ServerOneApplication.java
@@ -1,17 +1,13 @@
 package com.ilzf.server1;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Api("server1")
-@RestController
+@SpringBootApplication
+@EnableWebMvc
 public class ServerOneApplication {
-    
-    @ApiOperation(value = "测试云配置")
-    @GetMapping("/")
-    public String index() {
-        return "server1";
+    public static void main(String[] args) {
+        SpringApplication.run(ServerOneApplication.class, args);
     }
 }
--- spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/controller/IndexController.java
+++ spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/controller/IndexController.java
@@ -1,17 +1,21 @@
 package com.ilzf.server1.controller;
 
+import com.ilzf.starter.service.ILzfService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api("server1")
 @RestController
 public class IndexController {
+    @Autowired
+    ILzfService iLzfService;
 
     @ApiOperation(value = "测试云配置")
     @GetMapping("/")
     public String index() {
-        return "server1";
+        return iLzfService.getName();
     }
 }
