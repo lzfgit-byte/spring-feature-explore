5f5f2f3c3d31c2bc7ef860c54ab5da4c69ad8ddd
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-14 17:17:42 +0800

    spring-cloud-feign

--- spring-feature-spring-cloud/cloud-server2/src/main/java/com/ilzf/server2/ServerTwoApplication.java
+++ spring-feature-spring-cloud/cloud-server2/src/main/java/com/ilzf/server2/ServerTwoApplication.java
@@ -2,10 +2,12 @@ package com.ilzf.server2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableFeignClients(basePackages = "com.ilzf.feign.client.*")
 public class ServerTwoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerTwoApplication.class, args);
