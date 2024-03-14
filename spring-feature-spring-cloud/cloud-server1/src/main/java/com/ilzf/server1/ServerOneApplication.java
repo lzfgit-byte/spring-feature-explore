a59179b8aec34f7597d0b297fc1d6fa8fce89eef
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-14 13:54:01 +0800

    spring-cloud

--- spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/ServerOneApplication.java
+++ spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/ServerOneApplication.java
@@ -2,11 +2,12 @@ package com.ilzf.server1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ServerOneApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerOneApplication.class, args);
