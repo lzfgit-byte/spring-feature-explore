29688209513d0713dbf635b09dea664d5ea34050
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-14 11:17:20 +0800

    nacos

--- spring-feature-spring-cloud/nacos/bin/startup.cmd
+++ spring-feature-spring-cloud/nacos/bin/startup.cmd
@@ -22,8 +22,8 @@ rem removed the last 5 chars(which means \bin\) to get the base DIR.
 set BASE_DIR="%BASE_DIR:~0,-5%"
 
 set CUSTOM_SEARCH_LOCATIONS=file:%BASE_DIR%/conf/
-
-set MODE="cluster"
+@REM cluster standalone
+set MODE="standalone"
 set FUNCTION_MODE="all"
 set SERVER=nacos-server
 set MODE_INDEX=-1
