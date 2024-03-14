3254cec44e039228ed9f459c00356720417bd076
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-14 15:21:49 +0800

    spring-cloud

--- spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/controller/IndexController.java
+++ spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/controller/IndexController.java
@@ -1,16 +1,19 @@
 package com.ilzf.server1.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.ilzf.starter.service.ILzfService;
 import com.ilzf.util.LogUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api("server1")
 @RestController
+@RefreshScope
 public class IndexController {
     @Autowired
     ILzfService iLzfService;
@@ -20,7 +23,7 @@ public class IndexController {
     @ApiOperation(value = "测试云配置")
     @GetMapping("/")
     public String index() {
-        LogUtil.log(text);
+        LogUtil.log(this.text);
         return iLzfService.getName();
     }
 }
