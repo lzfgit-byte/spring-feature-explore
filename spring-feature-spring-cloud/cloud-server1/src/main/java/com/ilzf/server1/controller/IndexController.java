216309c67dbdea62b9aa9792fdfdcb10ffc7d8e1
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-15 15:10:19 +0800

    spring-cloud-dynamic

--- spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/controller/IndexController.java
+++ spring-feature-spring-cloud/cloud-server1/src/main/java/com/ilzf/server1/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.ilzf.server1.controller;
 
-import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.ilzf.feign.client.serverTwo.api.ServerTwoFeign;
 import com.ilzf.starter.service.ILzfService;
 import com.ilzf.util.LogUtil;
@@ -8,11 +7,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
