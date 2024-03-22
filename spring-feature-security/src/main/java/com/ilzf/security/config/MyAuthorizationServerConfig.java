690033d26c7d33aa9e4db41760720475396a678e
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-22 16:16:45 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/config/MyAuthorizationServerConfig.java
+++ spring-feature-security/src/main/java/com/ilzf/security/config/MyAuthorizationServerConfig.java
@@ -2,14 +2,21 @@ package com.ilzf.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Configuration
-@EnableAuthorizationServer
+//@EnableAuthorizationServer
 public class MyAuthorizationServerConfig extends WebSecurityConfigurerAdapter {
+
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
---
+++ spring-feature-security/src/main/java/com/ilzf/security/manage/MyAccessDecisionManager.java
@@ -0,0 +1,33 @@
+package com.ilzf.security.manage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+@Slf4j
+public class MyAccessDecisionManager implements AccessDecisionManager {
+    @Override
+    public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+        log.info("decide");
+        return;
+    }
+
+    @Override
+    public boolean supports(ConfigAttribute attribute) {
+        log.info("decide1");
+        return true;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        log.info("supports2");
+        return true;
+    }
+}
