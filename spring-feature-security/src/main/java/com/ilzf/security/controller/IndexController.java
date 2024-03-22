55a8300a5514165ed1c9ff623560673785911e10
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-22 14:01:48 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/controller/IndexController.java
+++ spring-feature-security/src/main/java/com/ilzf/security/controller/IndexController.java
@@ -3,13 +3,19 @@ package com.ilzf.security.controller;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 @RestController("/")
 @Api("测试权限")
 public class IndexController {
     @GetMapping("/")
     public String index() {
-        
+
+        try (Jedis jedis = new Jedis("127.0.0.1", 6379)) {
+            jedis.get("");
+        } catch (Exception e) {
+
+        }
         return "a";
     }
 }
