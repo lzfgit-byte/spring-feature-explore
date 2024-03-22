3ec1623a8e080349f2266e2b801b4194d2731bfd
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-22 14:43:46 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/controller/IndexController.java
+++ spring-feature-security/src/main/java/com/ilzf/security/controller/IndexController.java
@@ -1,10 +1,14 @@
 package com.ilzf.security.controller;
 
+import com.ilzf.util.LogUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController("/")
 @Api("测试权限")
@@ -14,9 +18,27 @@ public class IndexController {
     StringRedisTemplate redisTemplate;
 
 
+    @ApiOperation("向redis存值")
     @GetMapping("/")
     public String index() {
-        redisTemplate.opsForValue().set("test", "123456");
+        redisTemplate.opsForValue().set("stringVal", "123456");
+        redisTemplate.opsForHash().put("hashVal", "key", "value");
+        redisTemplate.opsForHash().put("hashVal", "key2", "value2");
+        redisTemplate.opsForHash().put("hashVal", "key3", "value3");
+        redisTemplate.opsForList().leftPush("listVal", "1");
+        redisTemplate.opsForList().leftPush("listVal", "2");
+        redisTemplate.opsForList().leftPush("listVal", "3");
+        redisTemplate.opsForSet().add("setVal", "1", "2", "3", "4", "5", "5");
         return "a";
+    }
+
+    @ApiOperation("从redis取值")
+    @GetMapping("/index2")
+    public String index2() {
+        LogUtil.log(redisTemplate.opsForValue().get("stringVal"));
+        Objects.requireNonNull(redisTemplate.opsForSet().members("setVal")).forEach(LogUtil::log);
+        LogUtil.log(Objects.requireNonNull(redisTemplate.opsForHash().get("hashVal", "key3")).toString());
+        LogUtil.log(redisTemplate.opsForList().leftPop("listVal"));
+        return redisTemplate.opsForValue().get("stringVal");
     }
 }
