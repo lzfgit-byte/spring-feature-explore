e7e699957214b18bd7cfed3a0daf66ffbe250e85
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-25 09:22:04 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/config/MyAuthorizationServerConfig.java
+++ spring-feature-security/src/main/java/com/ilzf/security/config/MyAuthorizationServerConfig.java
@@ -3,12 +3,14 @@ package com.ilzf.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 //@EnableAuthorizationServer
+@EnableWebSecurity
 public class MyAuthorizationServerConfig extends WebSecurityConfigurerAdapter {
 
 //    @Bean
