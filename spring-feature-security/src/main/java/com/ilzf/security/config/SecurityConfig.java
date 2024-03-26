dac419d07f0c70cfc6c857b985156bb1b79cbd0f
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-26 17:35:15 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/config/SecurityConfig.java
+++ spring-feature-security/src/main/java/com/ilzf/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ilzf.security.config;
 
+import com.ilzf.security.handler.FailHandler;
+import com.ilzf.security.handler.SuccessHandler;
 import com.ilzf.util.LogUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                以上设置请求的认证
                 .and()
                 .formLogin().loginPage("/api/login")
+                .successHandler(new SuccessHandler()).failureHandler(new FailHandler())
                 .and()
                 .exceptionHandling()
                 // 认证失败返回401状态码，前端页面可以根据401状态码跳转到登录页面。
@@ -49,7 +52,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase()))
                 .and().cors()
                 // csrf是否决定禁用，请自行考量
-                .and().csrf().disable()
+                .and().csrf()
+//                .disable()
+                .and()
+                //记住我 ,customizers是对记住我这个程序的设置
+                .rememberMe(rememberMe -> rememberMe.key("myKey").tokenValiditySeconds(7 * 24 * 3600))
                 // 会匹配所有api开头的请求，多个请求匹配规则，以后出现的为主。在此处就是已requestMatcher的匹配为主
                 //判断给那些请求进行权限校验
 //                .antMatcher("/api/**")
