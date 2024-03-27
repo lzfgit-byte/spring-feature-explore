14e05e662b3518d452d15f9f2de2866596ca55f1
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-27 14:28:01 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/handler/SuccessHandler.java
+++ spring-feature-security/src/main/java/com/ilzf/security/handler/SuccessHandler.java
@@ -1,5 +1,6 @@
 package com.ilzf.security.handler;
 
+import com.ilzf.security.entity.MyUser;
 import com.ilzf.util.LogUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -8,10 +9,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         LogUtil.log("登录成功");
+        response.setContentType("application/json;charset=UTF-8");
+        MyUser principal = (MyUser) authentication.getPrincipal();
+
+        Map<String, Object> res = new HashMap<>();
+        res.put("msg", "登录成功");
+        res.put("code", 200);
+        res.put("data", principal);
+        response.getWriter().println(res);
     }
 }
