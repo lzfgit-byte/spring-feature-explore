b6884a1d58e301ed95fd14d2bfbde73adc1c054d
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-27 15:33:41 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/handler/SuccessHandler.java
+++ spring-feature-security/src/main/java/com/ilzf/security/handler/SuccessHandler.java
@@ -2,8 +2,13 @@ package com.ilzf.security.handler;
 
 import com.ilzf.security.entity.MyUser;
 import com.ilzf.util.LogUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,12 +17,28 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class SuccessHandler implements AuthenticationSuccessHandler {
+//    @Autowired
+//    private ClientDetailsService clientDetailsService;
+//    @Autowired
+//    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         LogUtil.log("登录成功");
         response.setContentType("application/json;charset=UTF-8");
         MyUser principal = (MyUser) authentication.getPrincipal();
+        //集成oauth2 生成bear token
+        //生成token
+        String clientId = request.getParameter("client_id");
+        String clientSecret = request.getParameter("client_secret");
+
+//        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+//        TokenRequest tokenRequest = new TokenRequest(new HashMap<>(), clientId, clientDetails.getScope(), "custom");
+//        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+//        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+//        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
 
         Map<String, Object> res = new HashMap<>();
         res.put("msg", "登录成功");
--- spring-feature-security/src/main/java/com/ilzf/security/service/MyUserDetailService.java
+++ spring-feature-security/src/main/java/com/ilzf/security/service/MyUserDetailService.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
- * 登录校验
+ * 登录校验,自定义身份校验
  */
 @Service
 public class MyUserDetailService implements UserDetailsService {
