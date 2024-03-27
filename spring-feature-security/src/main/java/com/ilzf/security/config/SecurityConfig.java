22d8be3f2a2bd09269ebf79dc9b32bc75cfcca0a
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-03-27 14:18:24 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/config/SecurityConfig.java
+++ spring-feature-security/src/main/java/com/ilzf/security/config/SecurityConfig.java
@@ -43,22 +43,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
 //                以上设置请求的认证
                 .and()
-                .formLogin().loginProcessingUrl("/api/login")
-                .successHandler(new SuccessHandler()).failureHandler(new FailHandler())
-                .and()
-                .logout().logoutUrl("/api/logout").permitAll()
-                .and()
                 .exceptionHandling()
                 // 认证失败返回401状态码，前端页面可以根据401状态码跳转到登录页面。
                 .authenticationEntryPoint((request, response, authException) ->
                         response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase()))
                 .and().cors()
-                // csrf是否决定禁用，请自行考量
-                .and().csrf()
-//                .disable()
-                .and()
+                // csrf是否决定禁用，请自行考量。开放接口及时permitAll。没有csrftoken，仍然会403
+                .and().csrf().disable()
                 //记住我 ,customizers是对记住我这个程序的设置
                 .rememberMe(rememberMe -> rememberMe.key("myKey").tokenValiditySeconds(7 * 24 * 3600))
+
                 // 会匹配所有api开头的请求，多个请求匹配规则，以后出现的为主。在此处就是已requestMatcher的匹配为主
                 //判断给那些请求进行权限校验
 //                .antMatcher("/api/**")
@@ -82,6 +76,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         return false;
                     }
                 })
+                //登录
+                .formLogin().loginProcessingUrl("/api/login")
+                .usernameParameter("userName").passwordParameter("password").permitAll()
+                .successHandler(new SuccessHandler()).failureHandler(new FailHandler())
+                .and()
+                //登出
+                .logout().logoutUrl("/api/logout").permitAll()
+                .and()
                 // 采用http 的基本认证.
                 .httpBasic();
 //        super.configure(http);
