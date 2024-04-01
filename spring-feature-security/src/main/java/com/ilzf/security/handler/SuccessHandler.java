5c54f2636b87cedaf7e9e27cecb40012aa0300eb
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-04-01 17:57:30 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/handler/SuccessHandler.java
+++ spring-feature-security/src/main/java/com/ilzf/security/handler/SuccessHandler.java
@@ -19,10 +19,10 @@ import java.util.Map;
 
 @Component
 public class SuccessHandler implements AuthenticationSuccessHandler {
-//    @Autowired
-//    private ClientDetailsService clientDetailsService;
-//    @Autowired
-//    private AuthorizationServerTokenServices authorizationServerTokenServices;
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -34,16 +34,17 @@ public class SuccessHandler implements AuthenticationSuccessHandler {
         String clientId = request.getParameter("client_id");
         String clientSecret = request.getParameter("client_secret");
 
-//        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
-//        TokenRequest tokenRequest = new TokenRequest(new HashMap<>(), clientId, clientDetails.getScope(), "custom");
-//        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
-//        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
-//        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+        TokenRequest tokenRequest = new TokenRequest(new HashMap<>(), clientId, clientDetails.getScope(), "custom");
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
 
         Map<String, Object> res = new HashMap<>();
         res.put("msg", "登录成功");
         res.put("code", 200);
         res.put("data", principal);
+        res.put("token", token);
         response.getWriter().println(res);
     }
 }
