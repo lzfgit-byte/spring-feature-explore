04c543ef3640b1887acb907191a4c54be22e0f28
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-04-02 17:34:58 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/entryPoint/MyResourceAuthenticationEntryPoint.java
+++ spring-feature-security/src/main/java/com/ilzf/security/entryPoint/MyResourceAuthenticationEntryPoint.java
@@ -3,6 +3,7 @@ package com.ilzf.security.entryPoint;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ilzf.util.LogUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -21,11 +22,12 @@ public class MyResourceAuthenticationEntryPoint implements AuthenticationEntryPo
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        LogUtil.log("MyResourceAuthenticationEntryPoint");
+        response.setContentType("application/json;charset=UTF-8");
         String message = authException.getMessage();
-        Map<String, String> res = new HashMap<>();
-        res.put("code", "401");
-        res.put("msg", "登录失效");
+        LogUtil.log("MyResourceAuthenticationEntryPoint", message);
+        Map<String, Object> res = new HashMap<>();
+        res.put("code", HttpStatus.UNAUTHORIZED.value());
+        res.put("msg", HttpStatus.UNAUTHORIZED.getReasonPhrase());
         response.getWriter().write(objectMapper.writeValueAsString(res));
     }
 }
