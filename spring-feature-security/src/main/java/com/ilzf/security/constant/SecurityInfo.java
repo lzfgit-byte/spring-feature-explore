2a91ec953651a78c86024ad1c08997915b45e8e3
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-04-02 18:05:27 +0800

    spring-feature-security

--- spring-feature-security/src/main/java/com/ilzf/security/constant/SecurityInfo.java
+++ spring-feature-security/src/main/java/com/ilzf/security/constant/SecurityInfo.java
@@ -1,8 +1,7 @@
 package com.ilzf.security.constant;
 
-import javax.annotation.Resource;
 
 public class SecurityInfo {
     public static final String RESOURCE_ID = "spring-explore-resourceId";
-    public static final Integer EXPIRE_DATE = 123456;
+    public static final Integer EXPIRE_DATE = 30;
 }
